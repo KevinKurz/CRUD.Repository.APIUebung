f6ec09a10ac47b053011205e221a1aa631b3482c
Kevin.Kurz <Kevin.Kurz@PLANB-PF48Q43K>
2024-04-30 15:53:32 +0200

    add unstaged files

---
+++ CRUD.xUnitTests/Core_ReservationService/ReservationServiceExpectedErrors.cs
@@ -0,0 +1,120 @@
+﻿using CRUD.Core.ReservationService;
+using CRUD.DataBank;
+using CRUD.DataStructures.DataModel;
+using CRUD.DataStructures.DTOs.ReservationDTO;
+using Moq;
+
+namespace CRUD.xUnitTests.Core_ReservationService
+{
+    public class ReservationServiceExpectedErrors
+    {
+        // Mocklist
+        private List<TableModel> mockList = new List<TableModel>()
+        {
+            new TableModel(2, "Narrentisch"),
+            new TableModel(5, "Prinzentisch"),
+            new TableModel(8, "Königstisch"),
+            new TableModel(10, "Göttertisch")
+        };
+        private ReservationRepository _fakeRepo;
+        public ReservationServiceExpectedErrors()
+        {
+            // Mock of JsonService
+            Mock<JsonService> mockService = new Mock<JsonService>(); // You need to mock the Class, which you do not want to be accessed by the test
+            mockService.Setup(m => m.SaveListAsJsonFile(mockList)); // Create a fakeMethod of SaveList
+            mockService.Setup(m => m.LoadListFromJsonFile()).Returns(mockList); // Create a fakeMethod of LoadList
+
+            _fakeRepo = new ReservationRepository(mockService.Object); // Crate a new instance of ReservationRepository with the "Fake-Jason-Service-class"
+        }
+
+        // ------------------------------------------------------------------
+        // Create Reservation
+        // ------------------------------------------------------------------
+        [Fact]
+        public void CORE_ReservationService_ReservationRepository_Create_NotimplementedException_ForStartime()
+        {
+            //Arrange
+            CreateReservationDto dto = new CreateReservationDto(1, "test", "1:00", "4:00", "1.1.1001");
+            CreateReservationDto dtoBetween = new CreateReservationDto(1, "test", "2:00", "5:00", "1.1.1001");
+            //Act
+            _fakeRepo.Create(dto);
+            //Assert
+            NotImplementedException ex = Assert.Throws<NotImplementedException>(() => _fakeRepo.Create(dtoBetween));
+            Assert.Contains("Your starttime is in range with an already existing reservation", ex.Message);
+        }
+
+        [Fact]
+        public void CORE_ReservationService_ReservationRepository_Create_NotimplementedException_ForEndtime()
+        {
+            //Arrange
+            CreateReservationDto dto = new CreateReservationDto(1, "test", "2:00", "6:00", "1.1.1001");
+            CreateReservationDto dtoBetween = new CreateReservationDto(1, "test", "1:00", "5:00", "1.1.1001");
+            //Act
+            _fakeRepo.Create(dto);
+            //Assert
+            NotImplementedException ex = Assert.Throws<NotImplementedException>(() => _fakeRepo.Create(dtoBetween));
+            Assert.Contains("Your endtime is in range with an already existing reservation", ex.Message);
+        }
+
+        [Fact]
+        public void CORE_ReservationService_ReservationRepository_Create_NotimplementedException_ModelSurroundsReservation()
+        {
+            //Arrange
+            CreateReservationDto dto = new CreateReservationDto(1, "test", "2:00", "6:00", "1.1.1001");
+            CreateReservationDto dtoBetween = new CreateReservationDto(1, "test", "1:00", "7:00", "1.1.1001");
+            //Act
+            _fakeRepo.Create(dto);
+            //Assert
+            NotImplementedException ex = Assert.Throws<NotImplementedException>(() => _fakeRepo.Create(dtoBetween));
+            Assert.Contains("Your reservation surrounds an already existing reservation", ex.Message);
+        }
+
+        // ------------------------------------------------------------------
+        // Update Reservation
+        // ------------------------------------------------------------------
+        [Fact]
+        public void CORE_ReservationService_ReservationRepository_Update_NotimplementedException_ModelSurroundsReservation()
+        {
+            //Arrange
+            ReservationModel listFiller = new ReservationModel(1, "test", "2:00", "4:00", "1.1.1001");
+            ReservationModel createDto = new ReservationModel(1, "test", "1:00", "4:00", "2.1.1001");
+            UpdateReservationDto updateDto = new UpdateReservationDto(1, "test", "1:00", "5:00", "1.1.1001");
+            //Act
+            mockList[0].Availability.Add(listFiller);
+            mockList[0].Availability.Add(createDto);
+            //Assert
+            NotImplementedException ex = Assert.Throws<NotImplementedException>(() => _fakeRepo.UpdateById(0, 1, updateDto));
+            Assert.Contains("Your reservation surrounds an already existing reservation", ex.Message);
+        }
+
+        [Fact]
+        public void CORE_ReservationService_ReservationRepository_Update_NotimplementedException_ForStarttime()
+        {
+            //Arrange
+            ReservationModel listFiller = new ReservationModel(1, "test", "2:00", "4:00", "1.1.1001");
+            ReservationModel createDto = new ReservationModel(1, "test", "2:00", "5:00", "2.1.1001");
+            UpdateReservationDto UpdateDto = new UpdateReservationDto(1, "test", "3:00", "5:00", "1.1.1001");
+            //Act
+            mockList[0].Availability.Add(listFiller);
+            mockList[0].Availability.Add(createDto);
+            //Assert
+            NotImplementedException ex = Assert.Throws<NotImplementedException>(() => _fakeRepo.UpdateById(0, 1, UpdateDto));
+            Assert.Contains("Your starttime collides with already existing reservationtimes", ex.Message);
+        }
+
+        [Fact]
+        public void CORE_ReservationService_ReservationRepository_Update_NotimplementedException_ForEndtime()
+        {
+            //Arrange
+            ReservationModel listFiller = new ReservationModel(1, "test", "2:00", "4:00", "1.1.1001");
+            ReservationModel createDto = new ReservationModel(1, "test", "2:00", "5:00", "2.1.1001");
+            UpdateReservationDto UpdateDto = new UpdateReservationDto(1, "test", "1:00", "3:00", "1.1.1001");
+            //Act
+            mockList[0].Availability.Add(listFiller);
+            mockList[0].Availability.Add(createDto);
+            //Assert
+            NotImplementedException ex = Assert.Throws<NotImplementedException>(() => _fakeRepo.UpdateById(0, 1, UpdateDto));
+            Assert.Contains("Your endtime collides with already existing reservationtimes", ex.Message);
+        }
+    }
+}
